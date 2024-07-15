3899696a34ffce0991e3aa1ea5258b6cde0ea7e6
Caleb Joseph <calebbjoseph@gmail.com>
2024-07-15 06:49:33 -0500

    added coment

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require('dotenv').config();
-// require("./configs/database");
+require("./configs/database");
+const authRoute = require('./routes/auth')
+const userRoute = require('./routes/user')
+const contactRoute = require('./routes/contact')
+const clientRoute = require('./routes/client')
+const categoryRoute = require('./routes/category')
 
 class App {
     constructor() {
@@ -32,11 +37,11 @@ class App {
             res.status(200).json('index');
         });
 
-        // this.app.use('/auth', require('./routes/auth'));
-        // this.app.use('/user', require('./routes/user'));
-        // this.app.use('/contact', require('./routes/contact'));
-        // this.app.use('/client', require('./routes/client'));
-        // this.app.use('/category', require('./routes/category'));
+        this.app.use('/auth', authRoute);
+        this.app.use('/user', userRoute);
+        this.app.use('/contact', contactRoute);
+        this.app.use('/client', clientRoute);
+        this.app.use('/category', categoryRoute);
 
         // error handler
         this.app.use((err, req, res, next) => {
