a428789e3bc0080d586efdb5cf561f326dca5e90
Caleb Joseph <calebbjoseph@gmail.com>
2024-07-15 07:16:07 -0500

    init

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
-// const cors = require("cors");
+const cors = require("cors");
 require('dotenv').config();
 require("./configs/database");
 
@@ -9,22 +7,22 @@ class App {
     constructor() {
         this.app = express();
         this.port = process.env.PORT || 3000;
-        // this.middlewares();
+        this.middlewares();
         this.routes();
         this.start();
     }
 
-    // middlewares() {
-    //     this.app.use(bodyParser.json());
-    //     this.app.use(bodyParser.urlencoded({ extended: false }));
-    //     this.app.use(cookieParser());
+    middlewares() {
+        this.app.use(require("body-parser"));
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(require("cookie-parser"));
 
-    //     this.app.use(cors({
-    //         origin: process.env.VITE_CORS,
-    //         methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    //         credentials: true
-    //     }));
-    // }
+        this.app.use(cors({
+            origin: process.env.VITE_CORS,
+            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            credentials: true
+        }));
+    }
 
     routes() {
         this.app.get('/', (req, res) => {
