dc3424818a87495a0673c064433a78ceda3be4e3
Caleb Joseph <calebbjoseph@gmail.com>
2024-07-15 08:14:22 -0500

    updated database code

--- configs/database.js
+++ configs/database.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
-
-// Ensure you have the environment variable set correctly
-console.log(process.env.DB_URI); // This is for debugging purposes
-console.log("Error from database");
-
-mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
-    .then(() => console.log('Connected successfully!'))
-    .catch(err => console.log('Connection error: ', err));
+mongoose.connect(process.env.DB_URI)
+    .then(res => console.log('Connected successfully!'))
+    .catch(err => console.log(err));
