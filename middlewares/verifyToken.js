c248a6e05c0ac457d0bd9ede4b1e7e4813ffd6d4
Caleb Joseph <calebbjoseph@gmail.com>
2024-07-30 09:31:00 -0500

    added strict mode

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -4,14 +4,20 @@ const User = require('../models/User');
 
 const verifyToken = async (req, res, next) => {
     const service = new Service();
-    const token = req.cookies.token;
-    if (!token) return res.status(401).json({error: 'Token not found'});
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (!decoded) return res.status(401).json({error: 'Token not verified'});
+    let token;
+    token = req.cookies.token;
 
-    // if token is verified, pass user to req
-    req.user = await service.getOne(User, decoded._id);
-    next();
+    if (!token) return res.status(401).json({error: 'Token not found'});
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decoded) return res.status(401).json({error: 'Token not verified'});
+
+        // if token is verified, pass user to req
+        req.user = await service.getOne(User, decoded._id).select('-password');
+        next();
+    } catch (e) {
+        res.status(401).json("Invalid token");
+    }
 }
 
 module.exports = verifyToken;
