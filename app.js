b1edadbc1c080e4617dae2139453389b336f11f1
Caleb Joseph <calebbjoseph@gmail.com>
2024-08-04 08:07:20 -0500

    altered cors

--- app.js
+++ app.js
@@ -20,16 +20,17 @@ class App {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
         this.app.use(cookieParser());
-    
+        
         this.app.use(cors({
-            origin: (origin, callback) => {
-                if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-                    callback(null, true);
-                } else {
-                    callback(new Error('Not allowed by CORS'));
-                }
-            },
-            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            // origin: (origin, callback) => {
+            //     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            //         callback(null, true);
+            //     } else {
+            //         callback(new Error('Not allowed by CORS'));
+            //     }
+            // },
+            // methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            origin: 'http://localhost:8080',
             credentials: true
         }));
     }
